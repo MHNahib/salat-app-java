d46547925d3851b4d0dcab46e91cb66503ec6815
M. H. Nahib <mhnahib@gmail.com>
2023-04-12 19:54:52 +0600

    updated api and the icon

--- app/src/main/java/com/example/salatapp/MainActivity.java
+++ app/src/main/java/com/example/salatapp/MainActivity.java
@@ -22,12 +22,15 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button fetchTime;
+//    Button fetchTime;
     TextView fajr_time_data,sunrise_time_data, dhuhr_time_data,asr_time_data,magrib_time_data,sunset_time_data,isha_time_data;
 
     @Override
@@ -36,21 +39,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // assign var
-        fetchTime = findViewById(R.id.btn_fetch_time);
+//        fetchTime = findViewById(R.id.btn_fetch_time);
 
         fajr_time_data= findViewById(R.id.fajr_time_data);
-        sunrise_time_data= findViewById(R.id.sunrise_time_data);
+//        sunrise_time_data= findViewById(R.id.sunrise_time_data);
         dhuhr_time_data= findViewById(R.id.dhuhr_time_data);
         asr_time_data= findViewById(R.id.asr_time_data);
         magrib_time_data= findViewById(R.id.magrib_time_data);
-        sunset_time_data= findViewById(R.id.sunset_time_data);
+//        sunset_time_data= findViewById(R.id.sunset_time_data);
         isha_time_data= findViewById(R.id.isha_time_data);
 
         RequestQueue queue = Volley.newRequestQueue(MainActivity.this);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
         LocalDate currentDate = LocalDate.now();
         String date = currentDate.format(formatter);
-        String url = "https://api.aladhan.com/v1/timingsByAddress/"+date+"?address=Dhaka,Bangladesh&method=8";
+        String url = "https://api.aladhan.com/v1/timingsByAddress/"+date+"?address=Dhaka,Bangladesh&method=1";
 
         // call api
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
@@ -68,18 +71,31 @@ public class MainActivity extends AppCompatActivity {
                     String maghrib = timings.getString("Maghrib");
                     String isha = timings.getString("Isha");
 
-                    fajr_time_data.setText("Fajar: "+fajr);
-                    sunrise_time_data.setText("Sunrise: "+sunrise);
-                    dhuhr_time_data.setText("Dhuhr: "+dhuhr);
-                    asr_time_data.setText("Asr: "+asr);
-                    sunset_time_data.setText("Sunset: "+sunset);
-                    magrib_time_data.setText("Maghrib: "+maghrib);
-                    isha_time_data.setText("Isha: "+isha);
+
+
+                    SimpleDateFormat timeFormater = new SimpleDateFormat("hh:mm");
+                    SimpleDateFormat timeFormaterAsAMPM = new SimpleDateFormat("hh:mm a");
+
+                    Date fazarTime =timeFormater.parse(fajr);
+                    Date dhuhrTime =timeFormater.parse(dhuhr);
+                    Date asrTime =timeFormater.parse(asr);
+                    Date maghribTime =timeFormater.parse(maghrib);
+                    Date ishaTime =timeFormater.parse(isha);
+
+                    fajr_time_data.setText(timeFormaterAsAMPM.format(fazarTime).toString());
+//                    sunrise_time_data.setText("Sunrise: "+sunrise);
+                    dhuhr_time_data.setText(timeFormaterAsAMPM.format(dhuhrTime).toString());
+                    asr_time_data.setText(timeFormaterAsAMPM.format(asrTime).toString());
+//                    sunset_time_data.setText("Sunset: "+sunset);
+                    magrib_time_data.setText(timeFormaterAsAMPM.format(maghribTime).toString());
+                    isha_time_data.setText(timeFormaterAsAMPM.format(ishaTime).toString());
 
                 } catch (JSONException e) {
                     // Handle the exception here
                     e.printStackTrace(); // This will print the error stack trace to the console
                     Toast.makeText(MainActivity.this, "Error!!!", Toast.LENGTH_SHORT).show();
+                } catch (ParseException e) {
+                    throw new RuntimeException(e);
                 }
 
             }
@@ -94,13 +110,13 @@ public class MainActivity extends AppCompatActivity {
 
 
         // on event click
-        fetchTime.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-
-
-            }
-        });
+//        fetchTime.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//
+//
+//
+//            }
+//        });
     }
 }
